c03342dccd4e600915ee562487b039d0d9cf25d6
Deehjula <babydeeh@gmail.com>
2023-02-16 12:33:58 -0800

    Puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,12 @@
 #include<stdio.h>
 /**
- * main - A programm that prints a line with puts function
- * return: 0 (Success)
- */
+ * main - A programm that prints a line with puts functio
+n
+* return: 0 (Success)
+*/
 int main(void)
 {
-puts("\"Programming is like building a mulilingual puzzle");
+puts("\"Programming is like building a mulilingual puzz
+le");
 return (0);
 }
