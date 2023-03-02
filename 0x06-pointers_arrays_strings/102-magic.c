3de78a2ce69e184a9c468c94087f0f8ea02a928e
Deehjula <babydeeh@gmail.com>
2023-03-02 10:58:54 -0800

    it takes sweat

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-int main(void)
+int  main(void)
 {
 int n;
 int a[5];
@@ -8,8 +8,7 @@ a[2] = 1024;
 p = &n;
 /*
 * write your line of code here...
-* Remember:
-* - you are not allowed to use a
+* Remember:* - you are not allowed to use a
 * - you are not allowed to modify p
 * - only one statement
 * - you are not allowed to code anything else than this line of code
